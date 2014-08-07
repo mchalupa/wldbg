aa81d240dc58e5f1ecc588890476344ebae75936
Marek Chalupa <mchqwerty@gmail.com>
2014-08-07 11:18:05 +0200

    pass: add parsing of arguments
    get names of passes and their arguments from argv[]

--- src/passes.c
+++ src/passes.c
@@ -37,15 +37,70 @@ load_pass(struct wldbg *wldbg, const char *path)
 	return -1;
 }
 
+static int
+count_args(int argc, const char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; ++count) {
+		if ((strcmp(argv[count], "--") == 0)
+			|| (strcmp(argv[count], ",") == 0))
+			break;
+
+		if (strrchr(argv[count], ','))
+			return count + 1;
+	}
+
+	return count;
+}
+
 int
 load_passes(struct wldbg *wldbg, int argc, const char *argv[])
 {
+	int i, rest, count, pass_num = 0;
+	char *comma;
+
 	dbg("Loading passes\n");
-	dbg("Not implemented yet!!\n");
 
-	int i;
-	for (i = 0; i < argc; ++i)
-		dbg("\targ: %s\n", argv[i]);
+	rest = argc;
+	while (rest) {
+		count = count_args(rest, argv + argc - rest);
 
-	exit(1);
+		if (count == 0) {
+			/* -- or , or no arguments left */
+			if (rest == 0)
+				break;
+
+			if (strcmp(argv[argc - rest], "--") == 0) {
+				wldbg->client.path = argv[argc - rest + 1];
+				wldbg->client.argv
+					= (char * const *) argv + argc - rest + 1;
+				wldbg->client.argc = rest - 1;
+#ifdef DEBUG
+				dbg("Program: %s, argc == %d\n",
+					wldbg->client.path,
+					wldbg->client.argc);
+				for (i = 0; i < wldbg->client.argc; ++i)
+					dbg("\targ[%d]: %s\n",
+						i, wldbg->client.argv[i]);
+#endif /* DEBUG */
+				break;
+			} else if (strcmp(argv[argc - rest], ",") == 0) {
+				/* just skip the comma and process next pass */
+				--rest;
+				continue;
+			}
+		} else {
+			++pass_num;
+
+			dbg("Pass %d:\n", pass_num);
+#ifdef DEBUG
+			for (i = 0; i < count; ++i) {
+				dbg("\targ[%d]: %s\n", i, argv[argc - rest + i]);
+			}
+#endif
+
+			rest -= count;
+		}
+	}
 }
